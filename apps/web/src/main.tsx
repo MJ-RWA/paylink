8bf172b8d86b27403457b5cab3c642733f6e1e4c
Mekjah <believeinsomething2421@gmail.com>
2026-04-20 11:12:54 +0100

    fix:redirecturl

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -13,6 +13,13 @@ createRoot(document.getElementById('root')!).render(
           ethereum: { createOnLogin: 'off' },
           solana: { createOnLogin: 'off' },
         },
+       
+        loginMethods: ['google', 'email'],
+         appearance: {
+          theme: 'light',
+          accentColor: '#0D9488',
+          logo: 'https://paylink001.vercel.app/favicon.ico',
+        },
       }}
     >
       <App />
